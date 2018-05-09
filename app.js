d95212e35e1fd4218cff2f503d441d524a80cbbe
Julia Gerlach <juliagerlach442@gmail.com>
2018-05-09 11:19:06 -0500

    updating project files

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("this works!");
+let hourlyRate = prompt("Please enter hourly pay rate");
+let hoursWorked = prompt("Please enter hours worked");
+var num1 = parseInt (hourlyRate, 10);
+var num2 = parseInt (hoursWorked, 10);
+
+console.log(num1 * num2);
+
+ 
